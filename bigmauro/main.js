c98e85977284c9d6592536b9ac1a9238cc383b02
colombiaconeccion <colombiaconeccion@googlemail.com>
2026-05-06 00:37:12 +0200

    Update main.js

--- bigmauro/main.js
+++ bigmauro/main.js
@@ -203,7 +203,7 @@ async function requestWakeLock() {
 }
 
 // ====================== FLIP COUNTDOWN ======================
-const launchDate = new Date("2026-05-06T00:00:00").getTime();
+const launchDate = new Date("2026-05-12T23:59:59").getTime();
 const flipElements = {
     days:    { top: document.getElementById("daysTop"),    bottom: document.getElementById("daysBottom") },
     hours:   { top: document.getElementById("hoursTop"),   bottom: document.getElementById("hoursBottom") },
